cdde0df75629d638db8f4dd88365a4d6e1f8beb9
Aman Prajapati <ap757162@gmail.com>
2025-03-03 12:40:13 +0530

    [Aman] Add. Program to flip the coin added.

---
+++ ControlFlowProblems/FlipCoin.js
@@ -0,0 +1,8 @@
+// Simulate a coin flip (0 = Heads, 1 = Tails)
+let coinFlip = Math.floor(Math.random() * 2);
+
+if (coinFlip === 0) {
+    console.log("Heads");
+} else {
+    console.log("Tails");
+}
