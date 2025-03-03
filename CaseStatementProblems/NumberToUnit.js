b31037c82d3df4e9d0ea42ff9d1856bc2e29d855
Aman Prajapati <ap757162@gmail.com>
2025-03-03 12:58:25 +0530

    [Aman] Add. Program to Number to Unit added.

---
+++ CaseStatementProblems/NumberToUnit.js
@@ -0,0 +1,22 @@
+// Read a number 
+let num = 100;
+
+switch (num) {
+    case 1:
+        console.log("Unit");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten Thousand");
+        break;
+    default:
+        console.log("Invalid Input! Enter numbers like 1, 10, 100, 1000, etc.");
+}
