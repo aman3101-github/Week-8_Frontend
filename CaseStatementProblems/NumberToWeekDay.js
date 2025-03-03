16fa6247842730df03197d986eea450c330bf6be
Aman Prajapati <ap757162@gmail.com>
2025-03-03 12:53:39 +0530

    [Aman] Add. Program to convert digit to Weekday added.

---
+++ CaseStatementProblems/NumberToWeekDay.js
@@ -0,0 +1,28 @@
+// Read a number 
+let day = 4;
+
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid Input! Enter a number between 1 and 7.");
+}
