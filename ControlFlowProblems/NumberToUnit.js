c51fee5b400690aeee025077b133266c795bfe7b
Aman Prajapati <ap757162@gmail.com>
2025-03-03 12:45:58 +0530

    [Aman] Add. Program to convert number to its unit place added.

---
+++ ControlFlowProblems/NumberToUnit.js
@@ -0,0 +1,16 @@
+// Read a number from command line argument
+let num = parseInt(process.argv[2]);
+
+if (num === 1) {
+    console.log("Unit");
+} else if (num === 10) {
+    console.log("Ten");
+} else if (num === 100) {
+    console.log("Hundred");
+} else if (num === 1000) {
+    console.log("Thousand");
+} else if (num === 10000) {
+    console.log("Ten Thousand");
+} else {
+    console.log("Invalid Input! Enter numbers: 1, 10, 100, 1000, etc.");
+}
